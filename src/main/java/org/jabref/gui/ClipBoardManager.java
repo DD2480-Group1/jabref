29e30ddd443d34b93e0c646fcc36c85a711fc729
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-03-02 23:03:56 +0100

    Feat: Add sanity check for null in ClipBoardManager
    To prevent copying null to the clipboard, we add a sanity check for null
    before setting the content to the clipboard.
    
    Link to DD2480-Group1/jabref#15

--- src/main/java/org/jabref/gui/ClipBoardManager.java
+++ src/main/java/org/jabref/gui/ClipBoardManager.java
@@ -171,7 +171,7 @@ public class ClipBoardManager {
         final ClipboardContent content = new ClipboardContent();
         BibEntryWriter writer = new BibEntryWriter(new FieldWriter(preferencesService.getFieldPreferences()), entryTypesManager);
         StringBuilder builder = new StringBuilder();
-        stringConstants.forEach(strConst -> builder.append(strConst.getParsedSerialization()));
+        stringConstants.forEach(strConst -> builder.append(strConst.getParsedSerialization() == null ? "" : strConst.getParsedSerialization()));
         String serializedEntries = writer.serializeAll(entries, BibDatabaseMode.BIBTEX);
         builder.append(serializedEntries);
         // BibEntry is not Java serializable. Thus, we need to do the serialization manually
